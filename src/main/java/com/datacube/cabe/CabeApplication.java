d5bc95378a30a02a3e0353aae356916a4d617161
CodeFarmerPK <CodeFarmer.PK@Gmail.com>
2023-04-08 11:22:45 +0800

    Init Cabe

---
+++ src/main/java/com/datacube/cabe/CabeApplication.java
@@ -0,0 +1,13 @@
+package com.datacube.cabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CabeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/datacube/cabe/CabeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datacube.cabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
